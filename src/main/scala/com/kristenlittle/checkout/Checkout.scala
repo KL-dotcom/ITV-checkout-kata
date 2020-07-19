e9e35ee1728d6eee75d483a98287c5fc553c609d
Krissy Little <kristenlittle1992@gmail.com>
2020-07-19 23:21:28 +0100

    working with offers

--- src/main/scala/com/kristenlittle/checkout/Checkout.scala
+++ src/main/scala/com/kristenlittle/checkout/Checkout.scala
@@ -3,13 +3,18 @@ package com.kristenlittle.checkout
 class Checkout {
 
   def CheckoutCalc(items:List[String], sku:List[StockKeepingUnit]) : BigDecimal = {
-      items.map { elem =>
-       val info = sku.find(_.item == elem)
-        info.get.price
-      }.sum
+
+    def getPrice(itemDetails: StockKeepingUnit) : BigDecimal = {
+      val itemAmount = items.count(_== itemDetails.item)
+      itemDetails.offer.map{
+        offer => ((itemAmount / offer.offerQuantity).floor * offer.offerPrice) + (itemAmount % (offer.offerQuantity) * itemDetails.price)
+      }.getOrElse(itemAmount * itemDetails.price)
+
+    }
+
+    sku.map(elem => getPrice(elem)).sum
   }
 
-
 }
-
-case class StockKeepingUnit(item:String, price: BigDecimal)
+case class StockKeepingUnit(item:String, price: BigDecimal, offer: Option[SkuOffer])
+case class SkuOffer(offerQuantity: Int, offerPrice: BigDecimal)
--- src/test/scala/com/kristenlittle/checkout/CheckoutSpec.scala
+++ src/test/scala/com/kristenlittle/checkout/CheckoutSpec.scala
@@ -7,16 +7,38 @@ class CheckoutSpec extends AnyFlatSpec with Matchers {
 val underTest = new Checkout()
 
   "CheckoutCalc" should "return with a price for one item in items list and sku list" in {
-    underTest.CheckoutCalc(List("a"), List(StockKeepingUnit("a", 50))) should equal(50)
+    underTest.CheckoutCalc(List("a"), List(StockKeepingUnit("a", 50, None))) should equal(50)
   }
 
  it should "return with a price for multiple items in items list, but only one in sku list" in {
-   underTest.CheckoutCalc(List("a", "a", "a", "a"), List(StockKeepingUnit("a", 50))) should equal(200)
+   underTest.CheckoutCalc(List("a", "a", "a", "a"), List(StockKeepingUnit("a", 50, None))) should equal(200)
  }
 
   it should "return with a price for multiple items in items list and multiple in sku list" in {
-    underTest.CheckoutCalc(List("a", "b", "c", "d"), List(StockKeepingUnit("a", 50), StockKeepingUnit("b", 30),
-      StockKeepingUnit("c", 20), StockKeepingUnit("d", 15))) should equal(115)
+    underTest.CheckoutCalc(List("a", "b", "c", "d"), List(StockKeepingUnit("a", 50, None), StockKeepingUnit("b", 30, None),
+      StockKeepingUnit("c", 20, None), StockKeepingUnit("d", 15, None))) should equal(115)
+  }
+
+  it should "return with an offer for consecutive items" in {
+    underTest.CheckoutCalc(List("a", "a", "a"), List(StockKeepingUnit("a", 50, Option(SkuOffer(3, 130))))) should equal(130)
+  }
+
+  it should "return with a price that is double the offer" in {
+    underTest.CheckoutCalc(List("a", "a", "a", "a", "a", "a"), List(StockKeepingUnit("a", 50, Option(SkuOffer(3, 130))))) should equal(260)
+  }
+
+  it should "return with amount for total of offer and the remaining item with consecutive items" in {
+    underTest.CheckoutCalc(List("a", "a", "a", "a"), List(StockKeepingUnit("a", 50, Option(SkuOffer(3, 130))))) should equal(180)
+  }
+
+  it should "return multiple offers for consecutive items" in {
+    underTest.CheckoutCalc(List("a", "a", "a", "b", "b","c", "d"), List(StockKeepingUnit("a", 50, Option(SkuOffer(3, 130))), StockKeepingUnit("b", 30, Option(SkuOffer(2, 45))),
+      StockKeepingUnit("c", 20, None), StockKeepingUnit("d", 15, None))) should equal(210)
+  }
+
+  it should "return with multiple offers for non-consecutive items" in {
+    underTest.CheckoutCalc(List("b","d", "a","b", "a", "c","a"), List(StockKeepingUnit("a", 50, Option(SkuOffer(3, 130))), StockKeepingUnit("b", 30, Option(SkuOffer(2, 45))),
+      StockKeepingUnit("c", 20, None), StockKeepingUnit("d", 15, None))) should equal(210)
   }
 
 }
